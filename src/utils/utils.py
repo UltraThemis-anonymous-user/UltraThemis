e493c2e44ba568a7b34280b7b3d89c262eee1e5d
UltraThemis-anonymous-user <51049046+UltraThemis-anonymous-user@users.noreply.github.com>
2023-09-07 19:37:14 +0800

    Add files via upload

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,1 +1,20 @@
+import os
+import shutil
+import librosa
 
+
+def create_file(path):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+        print("remove "+path)
+
+    os.makedirs(path)
+    print("create "+path)
+
+def load_audio(path, target_fs=None):
+    data, fs = librosa.load(path, sr=None)
+    print(path, end='\r')
+    if target_fs!=None and fs != target_fs:
+        data = librosa.resample(data,fs,target_fs)
+        fs = target_fs
+    return data, fs
